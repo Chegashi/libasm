ebb952886c39cf0778e7c0ea93aa5f10953586bd
chegashi <mochegri@student.1337.ma>
2021-05-15 18:54:59 +0000

    add tester

--- libasm/ft_strdup.s
+++ libasm/ft_strdup.s
@@ -1,2 +1,28 @@
-global _ft_strcmp
+global _ft_strdup
+extern _malloc
+; char	*ft_strdup(const char *src)
+; {
+; 	char	*p;
+; 	size_t	i;
 
+; 	i = 0;
+; 	p = (char *)malloc(sizeof(char) * ft_strlen(src) + 1);
+; 	if (p == 0)
+; 		return (0);
+; 	while (i < ft_strlen(src))
+; 	{
+; 		p[i] = src[i];
+; 		i++;
+; 	}
+; 	p[i] = '\0';
+; 	return (p);
+; }
+
+_ft_strdup:
+    mov rax , 0
+
+ft_strdup :
+    mov rax , rdi
+	call _ft_strlen
+	call ft_strcpy
+    ret
